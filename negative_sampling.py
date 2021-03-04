a14d18a7922c19034a93f45abfc94fd171d61259
Matthew Koh <mskoh52@gmail.com>
2021-03-04 11:03:11 -0500

    put it in a script to use ddp

--- negative_sampling.py
+++ negative_sampling.py
@@ -1,9 +1,79 @@
-import torch
-from pytorch_lightning import Trainer
+from pytorch_lightning import LightningModule, Trainer
 from pytorch_lightning.callbacks.model_checkpoint import ModelCheckpoint
+from torch import optim
 
-from word2vec.data import Word2VecDatamodule, get_data
-from word2vec.negative_sampling import NegativeSampler, SkipGramNegSampling
+
+from word2vec.vocab import Word2VecDatamodule, get_data
+
+import torch
+import torch.nn.functional as F
+from torch import nn
+from torch.distributions import Multinomial
+
+bce = F.binary_cross_entropy_with_logits
+
+
+def one_hot(inds, vocab_size):
+    return F.one_hot(inds, vocab_size).to(torch.float32)
+
+
+def loss_func(pos, neg):
+    dev = pos.device
+    return (
+        bce(p := pos.squeeze(), torch.ones(p.shape).to(dev)).sum()
+        + bce(n := neg.squeeze(), torch.zeros(n.shape).to(dev)).sum()
+    )
+
+
+def loss_func_literal(pos: torch.Tensor, neg):
+    p = pos.squeeze()
+    n = neg.squeeze()
+    return -torch.log(F.sigmoid(p)).mean() - torch.log(F.sigmoid(-n)).mean()
+
+
+class NegativeSampler:
+    def __init__(self, vocab_size, probabilities):
+        assert len(probabilities) == vocab_size
+        self.dist = Multinomial(1, probs=probabilities)
+
+    def __call__(self, shape):
+        return torch.where(self.dist.sample(shape))[-1]
+
+
+class SkipGramNegSampling(LightningModule):
+    def __init__(self, vocab_size, embedding_dim, lr):
+        super().__init__()
+        self.save_hyperparameters()
+        self.U = nn.Linear(self.hparams.vocab_size, self.hparams.embedding_dim)
+        self.V = nn.Linear(self.hparams.vocab_size, self.hparams.embedding_dim)
+
+    def forward(self, center, ctx, neg):
+        U = self.U
+        V = self.V
+
+        v = V(one_hot(center, V.in_features))[:, None, :].transpose(1, 2)
+        u = U(one_hot(ctx, U.in_features))
+        ũ = U(one_hot(neg, U.in_features))
+
+        return (u @ v, ũ @ v)
+
+    def configure_optimizers(self):
+        return optim.Adam(self.parameters(), lr=self.hparams.lr)
+
+    def _step(self, stage, batch):
+        pos, neg = self(*batch)
+        loss = loss_func(pos, neg)
+        self.log(f"{stage}_loss", loss)
+        return loss
+
+    def training_step(self, batch, _):
+        return self._step("train", batch)
+
+    def validation_step(self, batch, _):
+        return self._step("val", batch)
+
+    def test_step(self, batch, _):
+        return self._step("test", batch)
 
 
 def negative_sampling_main(
@@ -55,6 +125,29 @@ def negative_sampling_main(
 
 if __name__ == "__main__":
     import sys
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--lr", type=float, default=1e-3)
+    parser.add_argument("--context-size", type=int, default=2)
+    parser.add_argument("--neg-per-pos", type=int, default=5)
+    parser.add_argument("--n-epochs", type=int, default=10)
+    parser.add_argument("--batch-size", type=int, default=64)
+    parser.add_argument("--num-workers", type=int, default=0)
+    parser.add_argument("--embedding-dim", type=int, default=100)
+    parser.add_argument("--train-path", default="data/wikitext-2/wiki.train.tokens")
+    parser.add_argument("--val-path", default="data/wikitext-2/wiki.valid.tokens")
+    parser.add_argument("--test-path", default="data/wikitext-2/wiki.test.tokens")
+    args = parser.parse_args()
 
-    num_workers = int(sys.argv[1])
-    negative_sampling_main(num_workers=num_workers, batch_size=128, n_epochs=10)
+    negative_sampling_main(
+        lr=args.lr,
+        context_size=args.context_size,
+        neg_per_pos=args.neg_per_pos,
+        embedding_dim=args.embedding_dim,
+        n_epochs=args.n_epochs,
+        batch_size=args.batch_size,
+        num_workers=args.num_workers,
+        train_path=args.train_path,
+        val_path=args.val_path,
+        test_path=args.test_path,
+    )
